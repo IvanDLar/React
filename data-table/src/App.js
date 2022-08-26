c17113972dec716acd5fd570306d76536609e447
IvanDLar <ivandiaz.lara.m@gmail.com>
2022-08-25 21:55:25 -0500

    Update App.js

--- data-table/src/App.js
+++ data-table/src/App.js
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import data from "./mock-data.json";
 import Papa from "papaparse";
 
+//Transform the number based grade into a latter based grade
 export function grade_to_letter(grade) {
   switch (true) {
     case grade < 64:
@@ -32,6 +33,7 @@ export function grade_to_letter(grade) {
   }
 }
 
+//Transform the date into the american date format
 export function date_to_american(date) {
   const day = date.split("/")[0];
   const month = date.split("/")[1];
@@ -39,17 +41,20 @@ export function date_to_american(date) {
   return month + "/" + day + "/" + year;
 }
 
+//Modify the full name
 export function remove_surname(fullName) {
   const firstName = fullName.split(" ")[0];
   const lastName = fullName.split(" ")[1];
   return firstName + " " + lastName;
 }
 
+//Modify the email
 export function change_mail(id) {
   const email = "@tec.mx";
   return id + email;
 }
 
+//Get the value of the column in order to return the correct modified values of the specific row
 export function format_line(i, val) {
   if (i === 0) {
     const index = val;
@@ -69,16 +74,6 @@ export function format_line(i, val) {
   }
 }
 const App = () => {
-  //React will update the app for us with this funciton
-
-  // const [contacts, setContacts] = useState(data);
-  // const [addFormData, setAddFormData] = useState({
-  //   fullName: "",
-  //   address: "",
-  //   phoneNumber: "",
-  //   email: "",
-  // });
-
   // State to store parsed data
   const [parsedData, setParsedData] = useState([]);
 
@@ -102,17 +97,7 @@ const App = () => {
           const keyNames = Object.keys(d);
 
           rowsArray.push(keyNames);
-
-          // if (keyNames[1] === "Name") {
-          //   const regNames = Object.values(d)[1];
-          //   const firstName = regNames.split(" ")[0];
-          //   const lastName = regNames.split(" ")[1];
-          //   const fullName = firstName + " " + lastName;
-          //   console.log(firstName, lastName);
-          // }
           valuesArray.push(Object.values(d));
-
-          // valuesArray.push(Object.values(d));
         });
 
         // Parsed Data Response in array format
@@ -177,20 +162,8 @@ const App = () => {
                   // if (i === 0) {
                   //   const index = val;
 
+                  //Display the values of the i column
                   return <td key={i}>{format_line(i, val)}</td>;
-                  // } else if (i === 1) {
-                  //   const regularName = val;
-                  //   return <td key={i}>{remove_surname(regularName)}</td>;
-                  // } else if (i === 2) {
-                  //   const ID = val;
-                  //   return <td key={i}>{change_mail(ID)}</td>;
-                  // } else if (i === 3) {
-                  //   const date = val;
-                  //   return <td key={i}>{date_to_american(date)}</td>;
-                  // } else if (i === 4) {
-                  //   const grade = val;
-                  //   return <td key={i}>{grade_to_letter(grade)}</td>;
-                  // }
                 })}
               </tr>
             );
@@ -200,104 +173,5 @@ const App = () => {
     </div>
   );
 };
-
-//   const handleAddFormChange = (event) => {
-//     event.preventDefault();
-
-//     const fieldName = event.target.getAttribute("name");
-//     const fieldValue = event.target.value;
-
-//     const newFormData = { ...addFormData }; //Copy the existing form data
-//     newFormData[fieldName] = fieldValue; //Update object with the value that has been tiped
-
-//     setAddFormData(newFormData);
-//   };
-
-//   const handleAddFormSubmit = (event) => {
-//     event.preventDefault();
-
-//     const newContact = {
-//       id: nanoid(),
-//       fullName: addFormData.fullName,
-//       address: addFormData.address,
-//       phoneNumber: addFormData.phoneNumber,
-//       email: addFormData.email,
-//     };
-
-//     const newContacts = [...contacts, newContact];
-//     setContacts(newContacts);
-//   };
-
-//   return (
-//     <div className="container">
-//       <table className="table table-success table-striped">
-//         <thead>
-//           <tr>
-//             <th>Name</th>
-//             <th>Address</th>
-//             <th>Phone Number</th>
-//             <th>Email</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {/*As the mpa function flips through the contacts array, it will give
-//           us access to the current contact object it is on and passes it to our
-//           function as a variable, that we named contact*/}
-//           {contacts.map((contact) => (
-//             <tr>
-//               <td>{contact.fullName}</td>
-//               <td>{contact.address}</td>
-//               <td>{contact.phoneNumber}</td>
-//               <td>{contact.email}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//       <h2>Upload File</h2>
-//       <div>
-//         {/* File Uploader */}
-//         <input
-//           type="file"
-//           name="file"
-//           accept=".csv"
-//           onChange={changeHandler}
-//           style={{ display: "block", margin: "10px auto" }}
-//         />
-//       </div>
-//       <h2>Add a Contact</h2>
-//       <form onSubmit={handleAddFormSubmit}>
-//         <input
-//           type="text"
-//           name="fullName"
-//           required="required"
-//           placeholder="Enter name"
-//           onChange={handleAddFormChange}
-//         ></input>
-//         <input
-//           type="text"
-//           name="address"
-//           required="required"
-//           placeholder="Enter address"
-//           onChange={handleAddFormChange}
-//         ></input>
-//         <input
-//           type="text"
-//           name="phoneNumber"
-//           required="required"
-//           placeholder="Enter a phone number"
-//           onChange={handleAddFormChange}
-//         ></input>
-//         <input
-//           type="email"
-//           name="email"
-//           required="required"
-//           placeholder="Enter email"
-//           onChange={handleAddFormChange}
-//         ></input>
-//         <button type="submit">Add</button>
-//       </form>
-//     </div>
-//   );
-// };
 
 export default App;
